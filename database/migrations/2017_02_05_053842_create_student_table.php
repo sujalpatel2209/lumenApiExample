8e1b53ea5307c2c6a295cad5f83bfedf29653756
sujalpatel2209 <sujalpatel022@gmail.com>
2017-02-05 12:27:24 +0530

    initial commit

---
+++ database/migrations/2017_02_05_053842_create_student_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id')->unsign();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('birthdate');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
